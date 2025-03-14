272a49bc1060e7268dfeaf681b68e3ef32c0f2e6
deadmedusa <zhidkov@hotmail.com>
2025-03-14 20:27:43 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "node_sg" {
   }
 
   ingress {
-    from_path   = 443
+    from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
